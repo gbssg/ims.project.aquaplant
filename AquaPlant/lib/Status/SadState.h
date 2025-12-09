573280d5cf64ee7b36d6c698aa4934c5db3983e5
Pablo <pedrodelpiedro@gmail.com>
2025-12-09 14:06:57 +0100

    Implement the SadState class

--- AquaPlant/lib/Status/SadState.h
+++ AquaPlant/lib/Status/SadState.h
@@ -1,1 +1,25 @@
 #pragma once
+
+#include <Arduino.h>
+#include "OLED.h"
+#include "LCD.h"
+#include "MD.h"
+// Class
+#include "IState.h"
+
+class HappyState : public State
+{
+public:
+    void RenderLCD()
+    {
+        LCDSad();
+    };
+    void RenderOLED()
+    {
+        emojiSad2();
+    };
+    void ExecuteMD()
+    {
+        MDLoopSad();
+    };
+};
