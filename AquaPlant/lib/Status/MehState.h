fc5f5f01986fcdba25a9c72a94fc03ee81db5701
Pablo <pedrodelpiedro@gmail.com>
2025-12-09 14:06:40 +0100

    Implement the MehState class

--- AquaPlant/lib/Status/MehState.h
+++ AquaPlant/lib/Status/MehState.h
@@ -1,1 +1,25 @@
 #pragma once
+
+#include <Arduino.h>
+#include "OLED.h"
+#include "LCD.h"
+#include "MD.h"
+// Class
+#include "IState.h"
+
+class MehState : public State
+{
+public:
+    void RenderLCD()
+    {
+        LCDMeh();
+    };
+    void RenderOLED()
+    {
+        emojiMeh2();
+    };
+    void ExecuteMD()
+    {
+        MDLoopMeh();
+    };
+};
